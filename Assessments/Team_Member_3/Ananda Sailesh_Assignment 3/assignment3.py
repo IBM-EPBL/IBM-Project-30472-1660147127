0ea900d04c034221df8d237a7360b5958e9488e8
Arun-3013 <karun3013@gmail.com>
2022-10-04 17:47:46 +0530

    Assignment 3 uploaded

---
+++ Assessments/Team_Member_3/Ananda Sailesh_Assignment 3/assignment3.py
@@ -0,0 +1,36 @@
+from gpiozero import LED
+import time
+
+
+#For raspberry pi 
+red = LED(5)
+yellow = LED(6)
+green = LED(13)
+
+
+
+while True:
+    
+    green.off()    #Execute in raspberry pi
+    red.on()       #Execute in raspberry pi
+
+    print("\033[91m Red light! GET ")
+    time.sleep(3)
+    print("\033[37m Blink!")
+    time.sleep(1)
+
+    red.off()     #Execute in raspberry pi
+    yellow.on()   #Execute in raspberry pi
+    
+    print("\033[93m Yellow light! SET ")
+    time.sleep(2)
+    print("\033[37m Blink!")
+    time.sleep(1)
+
+    yellow.off()   #Execute in raspberry pi
+    green.on()    #Execute in raspberry pi
+    
+    print("\033[92m Green light! GO")
+    time.sleep(3)
+    print("\033[37m Blink!")
+    time.sleep(1)
